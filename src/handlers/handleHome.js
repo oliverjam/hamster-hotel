99ab1322bd8caa6866bf32385c958839f47d9b7b
oliverjam <oliverjamesphillips@gmail.com>
2019-04-21 21:23:12 +0100

    Read & verify user cookie on homepage

--- src/handlers/handleHome.js
+++ src/handlers/handleHome.js
@@ -1,8 +1,13 @@
+const { parse } = require("cookie");
+const jwt = require("jsonwebtoken");
 const home = require("../templates/home");
 
-const handleHome = (_request, response) => {
+const handleHome = (request, response) => {
+  const { cookie } = request.headers;
+  const { user } = cookie ? parse(cookie) : {};
+  const { username } = user ? jwt.verify(user, process.env.SECRET) : {};
   response.writeHead(200, { "content-type": "text/html" });
-  const html = home();
+  const html = home({ username });
   response.end(html);
 };
 
