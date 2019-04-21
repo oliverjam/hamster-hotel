09b1d3cf9560f05a054b6049ac1d5fa4361e3a9b
oliverjam <oliverjamesphillips@gmail.com>
2019-04-21 17:56:49 +0100

    Test error and login routes

--- src/__test__/server.test.js
+++ src/__test__/server.test.js
@@ -1,12 +1,36 @@
 const supertest = require("supertest");
 const router = require("../router");
 
-test("home route returns a status code 200", () => {
-  return supertest(router)
-    .get("/")
-    .expect(200)
-    .expect("content-type", "text/html")
-    .then(response => {
-      expect(response.text).toMatch(/<title>Hamster Hotel<\/title>/);
-    });
+describe("Server routes", () => {
+  test("home route is rendered correctly", () => {
+    return supertest(router)
+      .get("/")
+      .expect(200)
+      .expect("content-type", "text/html")
+      .then(response => {
+        expect(response.text).toMatch(/<title>Hamster Hotel<\/title>/);
+      });
+  });
+  test("nonexistent route renders error", () => {
+    return supertest(router)
+      .get("/boobs")
+      .expect(404)
+      .expect("content-type", "text/html")
+      .then(response => {
+        expect(response.text).toMatch(
+          /<title>Not found | Hamster Hotel<\/title>/
+        );
+      });
+  });
+  test("login route returns JSON data for now", () => {
+    const formData = "username=oli&password=hello";
+    return supertest(router)
+      .post("/login")
+      .send(formData)
+      .expect(200)
+      .expect("content-type", "application/json")
+      .then(response => {
+        expect(response.body).toEqual({ username: "oli", password: "hello" });
+      });
+  });
 });
