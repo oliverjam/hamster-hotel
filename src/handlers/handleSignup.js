ae236b5b1f7d11f841c5155f1cdb079899c9247d
Samantha Dickeson <samlucydickeson@gmail.com>
2019-04-21 19:57:25 +0100

    add password hashing & storage

--- src/handlers/handleSignup.js
+++ src/handlers/handleSignup.js
@@ -1,5 +1,7 @@
 const { parse } = require("querystring");
+const bcrypt = require("bcrypt");
 const error = require("../templates/error");
+const db = require("../../database/connection");
 
 const handleSignup = (request, response) => {
   let data = "";
@@ -13,8 +15,27 @@ const handleSignup = (request, response) => {
       response.end(html);
     }
     const { username, password } = parse(data);
-    response.writeHead(200, { "content-type": "application/json" });
-    response.end(JSON.stringify({ username, password }));
+    bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => {
+        return db.query(
+          "INSERT INTO hamsters (username, password) VALUES ($1, $2)",
+          [username, hash]
+        );
+      })
+      .then(() => {
+        console.log("successful signup");
+        response.writeHead(302, { Location: "/" });
+        response.end();
+      })
+      .catch(err => {
+        console.log(err);
+        // error: duplicate key 23505 error code
+        response.writeHead(500, { "content-type": "text/html" });
+        const html = error({ status: 500 });
+        response.end(html);
+      });
   });
 };
 
